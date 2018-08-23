4d6e073376ed8a65d113f802aa595c9fc24097bd
jinweih <jinweih>
2018-08-23 16:56:25 +0800

    ad

--- SomeDemoFun/testJava/com/javen/src/CollectionDemo.java
+++ SomeDemoFun/testJava/com/javen/src/CollectionDemo.java
@@ -18,6 +18,19 @@ public interface Collection<E> extends Iterable<E>
 public boolean add (E o) //向集合中插入对象
 public boolean addAll (Collection<? extends E> c) //将一个集合的内容插入进来
 public void clear() //清楚此集合中的所有元素
+public boolean contains(Object o) //判断某一个对象是否在集合中存在
+public boolean containsAll(Collection<?> c) //判断一组对象是否在集合中存在
+public boolean equals(Object o) //对象比较
+public int hashCode() //哈希码
+public boolean isEmpty() //集合是否为空
+public Iterator<E> iterator() //为Iterator接口实例化
+public boolean remove(Object o) //删除指定对象
+public boolean removeAll(Collection<?> c)//删除一组对象
+public boolean retainAll(Collection<?> c)//保存指定内容
+public int size()//求出集合的大小
+public Object[] toArray() //将一个集合变为对象数组
+public<T> T[] toArray(T[] a) //指定好返回的对象数组类型
+
 public class CollectionDemo {
     List<String> list = new LinkedList<String>();
     Object
