af35a7f6fc114d4f4c84eb31e208b2d6e2053519
jinweih <jinwei>
2018-08-31 22:18:21 +0800

    Submit

---
+++ SomeDemoFun/testJava/com/javen/src/com/javen/reflect/GetClassDemo.java
@@ -0,0 +1,13 @@
+package com.javen.reflect;
+
+/**
+ * Created by jinweih on 2018/8/30.
+ */
+class Te{
+
+}
+public class GetClassDemo {
+    public static void main(String args[]) {
+        Class<T>
+    }
+}
